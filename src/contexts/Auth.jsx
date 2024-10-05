4e2a9209857bac81cba237fad3c8549c5d7adadc
JuliaYuki <julia.yuki.monte.ribeiro@gmail.com>
2024-10-04 21:06:05 -0300

    Implementacao de integracao front back login

--- src/contexts/Auth.jsx
+++ src/contexts/Auth.jsx
@@ -6,53 +6,65 @@ const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')));
+    const [token, setToken] = useState(() => localStorage.getItem('token')); 
     const navigate = useNavigate();
     
     useEffect(() => {
-        if (user) localStorage.setItem('user', JSON.stringify(user));
-        else localStorage.removeItem('user');
-    }, [user]);
+        if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            localStorage.setItem('token', token); 
+        } else {
+            localStorage.removeItem('user');
+            localStorage.removeItem('token');
+        }
+    }, [user, token]);
 
     async function signIn({ email_usuario, senha_usuario }) {
         try {
+
             const loginResponse = await axios.post('http://localhost:3000/login', { email_usuario, senha_usuario });
             const loginData = loginResponse.data;
-    
-            if (loginData && loginData.email_usuario) {
+
+            if (loginData && loginData.Token) {
                 const userResponse = await axios.get('http://localhost:3000/usuarios', {
-                    params: { email_usuario: loginData.email_usuario }
+                    headers: {
+                        Authorization: loginData.Token 
+                    },
+                    params: { email_usuario }
                 });
-    
+                // debugger;
                 const [userData] = userResponse.data;
-    
+
                 if (userData) {
-                    if (userData.senha_usuario === senha_usuario) {
-                        localStorage.setItem('user', JSON.stringify(userData));
-                        setUser(userData);
-                        navigate('/dashboard');
-                    } else {
-                        console.error('Senha incorreta');
-                        alert('Senha incorreta');
-                    }
+                    localStorage.setItem('user', JSON.stringify(userData));
+                    localStorage.setItem('token', loginData.Token); 
+                    setUser(userData);
+                    console.log(loginData)
+                    setToken(loginData.Token); 
+
+                    navigate('/dashboard');
                 } else {
-                    console.error('Usuário não encontrado');
                     alert('Usuário não encontrado');
                 }
+            } else {
+                alert('Falha na autenticação');
             }
         } catch (error) {
             console.error('Falha na autenticação', error);
-            alert('Falha na autenticação');
+            alert('Erro no login');
         }
     }
 
     function signOut() {
         localStorage.removeItem('user');
+        localStorage.removeItem('token');
         setUser(null);
+        setToken(null);
         navigate('/');
     }
 
     return (
-        <AuthContext.Provider value={{ user, signIn, signOut }}>
+        <AuthContext.Provider value={{ user, token, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     );
