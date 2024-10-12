096392ceb949164bf2360642094847c6814ca737
naty-c <nmfcagnani@gmail.com>
2024-10-12 02:43:12 -0300

    Estilização do carrossel

--- src/components/Card/ModalCardDestinos.jsx
+++ src/components/Card/ModalCardDestinos.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import './CardModalDestinos.css';
+import styles from './ModalCardDestinos.module.css';
 
 const ModalCardDestinos = ({ isOpen, onClose, destino }) => {
   if (!isOpen || !destino) return null;
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <h2 className='nomeMODAL'>{destino.nome}</h2>
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <span className={styles.close} onClick={onClose}>&times;</span>
+        <h2 className={styles.nomeModal}>{destino.nome}</h2>
         <p><strong>Categoria:</strong> {destino.categoria_destino}</p>
         <p><strong>CEP:</strong> {destino.cep_destino}</p>
         <p><strong>Cidade:</strong> {destino.cidade_destino}</p>
