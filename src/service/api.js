b01729fd7b6ab990c225f66d5045d1ba8d1fa50d
naty-c <nmfcagnani@gmail.com>
2024-10-13 21:56:55 -0300

    Alterando API BASE URL

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'https://m3p-backend-squad5-2.onrender.com';
 
 export const ENDPOINTS = {
     dashboard: `${API_BASE_URL}/`,
